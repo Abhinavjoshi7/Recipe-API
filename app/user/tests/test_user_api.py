003f92a28db030166a955584c0fedd24426035f6
Abhinavjoshi7 <write2abhij@gmail.com>
2024-04-04 23:44:31 -0600

    Unit test for token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -12,7 +12,7 @@ reverse() is a utility function provided by django.urls module
 that is used to dynamically build URLs by their {view} name.
 """
 CREATE_USER_URL = reverse('user:create')
-
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     """Create and return a new user."""
@@ -63,3 +63,36 @@ class PublicUserAPITests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
+    def test_create_token_for_user(self):
+        """Test generated token for valid credentials"""
+        user_details = {
+            'name': 'Test name',
+            'email': 'test@example.com',
+            'password': 'pwd123456789',
+        }
+        create_user(**user_details)
+        payload = {
+            'email' : user_details['email'],
+            'password' : user_details['password']
+        }
+        res = self.client.post(TOKEN_URL, payload)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """Test returns error if credentials invalid"""
+        create_user(email = 'test@example.com', password = 'goodpass')
+        payload = {'email': 'test@example.com', 'password': 'badpass'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test posting a blank password returns an error"""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
